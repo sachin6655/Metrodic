618a69c14d8bee3cf8d463d0616508c1040813b4
sachin6655 <sachin66@gmail.com>
2022-11-27 13:28:06 +0000

    API Commit

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBMatrodicFitness.Data;
+using EntityMetrodicFitness.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductController : ControllerBase
+    {
+        public readonly StoreContext _storecontext;
+        public ProductController(StoreContext storeContext)
+        {
+            _storecontext = storeContext;
+        }
+
+        [HttpGet]
+        /*public IActionResult Index()
+        {
+            var products = new Product{ 1, "one" }; 
+            return List<prod>
+        }*/
+
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+
+            var products = await _storecontext.Products.ToListAsync();
+
+            return Ok(products);
+        
+        
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+
+            return Ok(await _storecontext.Products.FindAsync(id));
+
+        }
+
+
+
+
+
+    }
+}
